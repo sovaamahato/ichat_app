60c4ecc3b90e7e3666998c427f6515e99938baa3
sova <mahatoshobha143@gmail.com>
2023-06-02 23:57:19 +0545

    error while keyborad oping solved

--- lib/Authentication/sign_up_page.dart
+++ lib/Authentication/sign_up_page.dart
@@ -34,118 +34,120 @@ class _SignUpPageState extends State<SignUpPage> {
           ])),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 30,
-              ),
-        
-             
-        
-              SizedBox(
-                height: 20,
-              ),
-        
-              Text(
-                "Create account Now",
-                style: GoogleFonts.alike(fontSize: 25),
-              ),
-              Text(
-                "to get Started",
-                style: GoogleFonts.alikeAngular(fontSize: 30),
-              ),
-               SizedBox(
-                height: 45,
-              ),
-              //username enter------------------------
-              MyTextField(
-                  hintText: "UserName",
-                  leadingIcon: Icon(
-                    Icons.person_outlined,
-                  ),
-                  textController: userNameController,
-                  obscure: false),
-        
-              SizedBox(
-                height: 15,
-              ),
-              //email enter------------------------
-              MyTextField(
-                  hintText: "Email",
-                  leadingIcon: Icon(
-                    Icons.email_outlined,
-                  ),
-                  textController: emailController,
-                  obscure: false),
-        
-              SizedBox(
-                height: 15,
-              ),
-        //password---------------------------
-              MyTextField(
-                  hintText: "Password",
-                  leadingIcon: Icon(
-                    Icons.email_outlined,
-                  ),
-                  textController: passwordController,
-                  obscure: true),
-              SizedBox(
-                height: 40,
-              ),
-        //login button----------------------
-              GestureDetector(
-                 onTap: () {
-
-                  if(userNameController.text.isNotEmpty &&
-                  emailController.text.isNotEmpty&&
-                  passwordController.text.isNotEmpty){
-                    authenticationController.createAccountForNewUser(
-                    userNameController.text,
-                    emailController.text,
-                      passwordController.text);
-
-                  }
-                  
-                        
-                      },
-                child: Container(
-                  width: MediaQuery.of(context).size.width - 10,
-                  height: 50,
-                  decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      borderRadius: BorderRadius.circular(19)),
-                  child: Center(
-                      child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      "Sign Up ",
-                      style: GoogleFonts.alike(
-                        fontSize: 20,
-                        
-                      ),
-                    ),
-                  )),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 30,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [Text("Aready have an account? "),
+          
+               
+          
+                SizedBox(
+                  height: 20,
+                ),
+          
+                Text(
+                  "Create account Now",
+                  style: GoogleFonts.alike(fontSize: 25),
+                ),
+                Text(
+                  "to get Started",
+                  style: GoogleFonts.alikeAngular(fontSize: 30),
+                ),
+                 SizedBox(
+                  height: 45,
+                ),
+                //username enter------------------------
+                MyTextField(
+                    hintText: "UserName",
+                    leadingIcon: Icon(
+                      Icons.person_outlined,
+                    ),
+                    textController: userNameController,
+                    obscure: false),
+          
+                SizedBox(
+                  height: 15,
+                ),
+                //email enter------------------------
+                MyTextField(
+                    hintText: "Email",
+                    leadingIcon: Icon(
+                      Icons.email_outlined,
+                    ),
+                    textController: emailController,
+                    obscure: false),
+          
+                SizedBox(
+                  height: 15,
+                ),
+          //password---------------------------
+                MyTextField(
+                    hintText: "Password",
+                    leadingIcon: Icon(
+                      Icons.email_outlined,
+                    ),
+                    textController: passwordController,
+                    obscure: true),
+                SizedBox(
+                  height: 40,
+                ),
+          //login button----------------------
                 GestureDetector(
-                  onTap: ()
-                  {
-                    Get.to(LoginPage());
-                  },
-                  child: Text(" Login Now",style: TextStyle(fontSize: 17,
-                  color: Color.fromARGB(255, 202, 182, 236)),),
-                )],
-              )
-            ],
+                   onTap: () {
+        
+                    if(userNameController.text.isNotEmpty &&
+                    emailController.text.isNotEmpty&&
+                    passwordController.text.isNotEmpty){
+                      authenticationController.createAccountForNewUser(
+                      userNameController.text,
+                      emailController.text,
+                        passwordController.text);
+        
+                    }
+                    
+                          
+                        },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width - 10,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(19)),
+                    child: Center(
+                        child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Sign Up ",
+                        style: GoogleFonts.alike(
+                          fontSize: 20,
+                          
+                        ),
+                      ),
+                    )),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Text("Aready have an account? "),
+                  GestureDetector(
+                    onTap: ()
+                    {
+                      Get.to(LoginPage());
+                    },
+                    child: Text(" Login Now",style: TextStyle(fontSize: 17,
+                    color: Color.fromARGB(255, 202, 182, 236)),),
+                  )],
+                )
+              ],
+            ),
           ),
         ),
       ),
