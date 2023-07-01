9973ea54781688dd5a8bef398440bf4543c63b88
sova <mahatoshobha143@gmail.com>
2023-07-01 22:10:38 +0545

    fuction for checking existing user while creating new user is defined

--- lib/api/apis.dart
+++ lib/api/apis.dart
@@ -1,7 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 
 class APIs {
+
+  //for authentication 
+  static FirebaseAuth auth = FirebaseAuth.instance; 
   //for accessing cloud firestore database
   static FirebaseFirestore firestore=FirebaseFirestore.instance;
+
+  //for checking if user exists or not
+  static Future<bool> userExists()async{
+    return (await firestore.collection("users").doc(auth.currentUser!.uid).get()).exists;
+  }
 }
